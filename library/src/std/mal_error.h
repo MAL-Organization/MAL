4b99cabc16aaad257c40ec8ac43a50b33e73576b
oallaire <olivier.allaire@gmail.com>
2015-10-18 09:49:01 -0400

    ref issue #2 - Documented the pool util to add support for doxygen.

--- library/src/std/mal_error.h
+++ library/src/std/mal_error.h
@@ -1,13 +1,9 @@
-/*
- * mal_erno.h
- *
- *  Created on: May 3, 2015
- *      Author: Olivier
- */
-/*
- * Copyright (c) 2015 Olivier Allaire
- *
- * This file is part of MAL.
+/**
+ * @file mal_error.h
+ * @author Olivier Allaire
+ * @date May 3 2015
+ * @copyright Copyright (c) 2015 Olivier Allaire
+ * @par This file is part of MAL.
  *
  * MAL is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -21,20 +17,24 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with MAL.  If not, see <http://www.gnu.org/licenses/>.
+ * @brief Defines possible error codes returned by all functions in MAL.
  */
 
 #ifndef STD_MAL_ERNO_H_
 #define STD_MAL_ERNO_H_
 
+/**
+ * Possible error codes returned by all MAL functions.
+ */
 typedef enum {
-	MAL_ERROR_OK = 0,
-	MAL_ERROR_HARDWARE_INVALID = -1,
-	MAL_ERROR_CLOCK_ERROR = -2,
-	MAL_ERROR_HARDWARE_UNAVAILABLE = -3,
-	MAL_ERROR_INIT_FAILED = -4,
-	MAL_ERROR_FULL = -5,
-	MAL_ERROR_EMPTY = -6,
-	MAL_ERROR_USER = -7
+	MAL_ERROR_OK = 0,                   //!< MAL_ERROR_OK
+	MAL_ERROR_HARDWARE_INVALID = -1,    //!< MAL_ERROR_HARDWARE_INVALID
+	MAL_ERROR_CLOCK_ERROR = -2,         //!< MAL_ERROR_CLOCK_ERROR
+	MAL_ERROR_HARDWARE_UNAVAILABLE = -3,//!< MAL_ERROR_HARDWARE_UNAVAILABLE
+	MAL_ERROR_INIT_FAILED = -4,         //!< MAL_ERROR_INIT_FAILED
+	MAL_ERROR_FULL = -5,                //!< MAL_ERROR_FULL
+	MAL_ERROR_EMPTY = -6,               //!< MAL_ERROR_EMPTY
+	MAL_ERROR_USER = -7                 //!< MAL_ERROR_USER
 } mal_error_e;
 
 #endif /* STD_MAL_ERNO_H_ */
--- library/src/utils/mal_pool.h
+++ library/src/utils/mal_pool.h
@@ -1,13 +1,9 @@
-/*
- * mal_pool.h
- *
- *  Created on: Oct 4, 2015
- *      Author: Olivier
- */
-/*
- * Copyright (c) 2015 Olivier Allaire
- *
- * This file is part of MAL.
+/**
+ * @file mal_pool.h
+ * @author Olivier Allaire
+ * @date October 4 2015
+ * @copyright Copyright (c) 2015 Olivier Allaire
+ * @par This file is part of MAL.
  *
  * MAL is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -21,6 +17,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with MAL.  If not, see <http://www.gnu.org/licenses/>.
+ * @brief Object pool utility. This will allow to emulate a memory heap while
+ * having a deterministic memory usage.
  */
 
 #ifndef UTILS_MAL_POOL_H_
@@ -30,21 +28,55 @@
 #include "std/mal_stdint.h"
 #include "std/mal_error.h"
 
+/**
+ * This structure is the base constituent of the pool. It is a basic container
+ * to track an object pointer and its state.
+ */
 typedef struct {
-	bool is_free;
-	void *object;
+	bool is_free; /**< A boolean value. It is true if it's free, false otherwise.*/
+	void *object; /**< A pointer to the object.*/
 } mal_pool_object_s;
 
+/**
+ * The pool object. This is a container for all the variables required to do
+ * all pool operations.
+ */
 typedef struct {
-	mal_pool_object_s *objects;
-	uint64_t object_size;
-	uint64_t size;
+	mal_pool_object_s *objects; /**< This is an array of #mal_pool_objects_s.*/
+	uint64_t object_size; /**< This is the size of each object stored by #mal_pool_object_s in bytes*/
+	uint64_t size; /**< The size of the pool. In other words, the size of the objects array.*/
 } mal_pool_s;
 
+/**
+ * @brief This function will initialise an instance of a pool object.
+ * @param object_pool This should be an array of #mal_pool_object_s.
+ * @param pool_size The number of #mal_pool_object_s that fits in ::object_pool.
+ * In other words, the size of the array.
+ * @param object_buffers This is an array of objects casted to bytes. It could
+ * be an array of a complex structure or simply an array of integers. pool_size
+ * should also indicate how many object can fit into the buffers.
+ * @param object_size The size, in bytes, of a single object.
+ * @param pool The pool to initialise.
+ * @return Returns #MAL_ERROR_OK upon completion.
+ */
 mal_error_e mal_pool_init(mal_pool_object_s *object_pool, uint64_t pool_size, uint8_t *object_buffers, uint64_t object_size, mal_pool_s *pool);
 
+/**
+ * @brief This function will attempt to allocate a free object.
+ * @param pool The pool of objects to allocate from.
+ * @param object If a free object is found, it will be stored in object.
+ * @return If an object is found, #MAL_ERROR_OK will be returned.
+ * #MAL_ERROR_EMPTY will be returned if no free object remains.
+ */
 mal_error_e mal_pool_allocate(mal_pool_s *pool, void **object);
 
+/**
+ * @brief This function will free an allocated object.
+ * @param pool The pool to free the object from.
+ * @param object The object to free.
+ * @return Will always return #MAL_ERROR_OK, even if the object is free or not
+ * found.
+ */
 mal_error_e mal_pool_free(mal_pool_s *pool, void *object);
 
 #endif /* UTILS_MAL_POOL_H_ */
