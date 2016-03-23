0508a29aa14407e1967b294babd3a7722eb04fce
oallaire <olivier.allaire@gmail.com>
2016-03-23 17:20:34 -0400

    ref issue #26 - Modified build process to include a version of the lib compiled with MinGW.

--- library/build-scripts/compile.ps1
+++ library/build-scripts/compile.ps1
@@ -99,15 +99,81 @@ function RetreiveExcludeFromXML {
 		if ($storagemodules_xml[$i].moduleId -eq "cdtBuildSystem") {
 			if ($storagemodules_xml[$i].configuration.name -eq $build_config) {
 				$entries_xml = $storagemodules_xml[$i].configuration.sourceEntries.entry
+				$pure_exclusions_array = New-Object "System.Collections.Generic.List[System.String]"
+				$compiled_exclusions_array = New-Object "System.Collections.Generic.List[System.String]"
 				[string[]]$exclusions_array = @()
-				for ($j=0; $j -le $entries_xml.length-1; $j++) {
-					if ($entries_xml[$j].name -ne "" -and $entries_xml[$j].excluding -ne $null ) {
-						$exclusion = $entries_xml[$j].excluding.Replace("/","\").Split('|',[System.StringSplitOptions]::RemoveEmptyEntries)
-						for ($k=0; $k -le $exclusion.length-1; $k++) {
-							$exclusion[$k] = "*"+$exclusion[$k]+"*"
+				# Find all pure exclusions paths (must have an empty name attribute and an exclude value)
+				for ($j=0; $j -lt $entries_xml.length; $j++) {
+					if ($entries_xml[$j].name -eq "" -and $entries_xml[$j].excluding -ne $null ) {
+						$exclusions = $entries_xml[$j].excluding.Replace("/","\").Split('|',[System.StringSplitOptions]::RemoveEmptyEntries)
+						for ($k=0; $k -lt $exclusions.length; $k++) {
+							$pure_exclusions_array.Add($exclusions[$k])
 						}
-						$exclusions_array += $exclusion
 					}
+				}
+				# Find entries that remove pure exclusion paths (must have a name that is in the pure exclusion paths and must have a null exclusion)
+				for ($j=0; $j -lt $entries_xml.length; $j++) {
+					# Check if the name is in the pure exclusions
+					$exclusion_found = $false
+					if ($entries_xml[$j].name -eq $null) {
+						continue
+					}
+					$exclusion_found = $false
+					for ($k=0; $k -lt $pure_exclusions_array.Count; $k++) {
+						if ($entries_xml[$j].name -eq $pure_exclusions_array[$k]) {
+							$exclusion_found = $true
+							break
+						}
+					}
+					if ($exclusion_found -eq $false) {
+						continue
+					}
+					# Make sure excluding is not an attribute for this entry
+					if ($entries_xml[$j].excluding -ne $null ) {
+						continue
+					}
+					# Remove pure exclusion
+					$pure_exclusions_array.Remove($entries_xml[$j].name)
+				}
+				
+				# Find entries that specify a subset of a pure path (must have a name that is the pure exclusion path and must have a valid exclusion)
+				for ($j=0; $j -lt $entries_xml.length; $j++) {
+					# Check if the name is in the pure exclusions
+					$exclusion_found = $false
+					if ($entries_xml[$j].name -eq $null) {
+						continue
+					}
+					$exclusion_found = $false
+					for ($k=0; $k -lt $pure_exclusions_array.Count; $k++) {
+						if ($entries_xml[$j].name -eq $pure_exclusions_array[$k]) {
+							$exclusion_found = $true
+							break
+						}
+					}
+					if ($exclusion_found -eq $false) {
+						continue
+					}
+					# Make sure excluding is an attribute for this entry
+					if ($entries_xml[$j].excluding -eq $null ) {
+						continue
+					}
+					# Remove pure exclusion
+					$pure_exclusions_array.Remove($entries_xml[$j].name)
+					# Add paths to compiled exclusions
+					$exclusions = $entries_xml[$j].excluding.Replace("/","\").Split('|',[System.StringSplitOptions]::RemoveEmptyEntries)
+					for ($k=0; $k -lt $exclusions.length; $k++) {
+						$compiled_exclusions_array.Add($exclusions[$k])
+					}
+				}
+				
+				# Transfer remaining pure paths in the compiled list
+				for ($j = 0; $j -lt $pure_exclusions_array.Count; $j++) {
+					$compiled_exclusions_array.Add($pure_exclusions_array[$j])
+				}
+				
+				# Build result
+				for ($j=0; $j -lt $compiled_exclusions_array.Count; $j++) {
+					$exclusions_array += "*"+$compiled_exclusions_array[$j]+"*"
 				}
 				#Exclusion found stoping
 				break
@@ -172,16 +238,16 @@ $exe = $eclipse_path + "\eclipsec.exe"
 for ($i=0; $i -le $optimization_levels.length-1; $i++){
 	$level = $optimization_levels[$i]
 	ChangeOptimizationLevelInXML $xml_path $base_optimization_level $level $build_configs
-	Write-Output "Building library with optimization level $level..."
+	Write-Host "Building library with optimization level $level..."
 	#Launch build and capture output to exit on errors
 	$output = & $exe $build_parameters *>&1
 	if ($output -like "*Workspace already in use!*") {
-		Write-Output "ERROR: Workspace already in use! - Please close Eclipse"
+		Write-Host "ERROR: Workspace already in use! - Please close Eclipse"
 		Exit 1
 		break
 	}
 	elseif ($output -like "*Skipping*") {
-		Write-Output "ERROR: Wrong workspace path"
+		Write-Host "ERROR: Wrong workspace path"
 		Exit 1
 		break
 	}
