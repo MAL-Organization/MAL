dfb29a032625fe626402e0b8ade887fafe743193
Olivier Allaire <olivier.allaire@gmail.com>
2019-07-04 15:00:04 -0400

    Adding GNU and STM32F0 implementation of set frequency

--- library/src/hspec/gnu/mal_hspec_gnu_timer.c
+++ library/src/hspec/gnu/mal_hspec_gnu_timer.c
@@ -197,10 +197,17 @@ mal_error_e mal_hspec_gnu_timer_do_input_capture_callback(mal_timer_e timer,  ma
 }
 
 mal_error_e mal_timer_init_pwm(mal_timer_init_pwm_s *init, mal_timer_s *handle, mal_timer_pwm_s *pwm_handle) {
+    MAL_DEFS_UNUSED(pwm_handle);
     handle->timer = init->timer;
     handle->used = true;
     gnu_timers[init->timer].timer = init->timer;
     gnu_timers[init->timer].count = 0;
+    return MAL_ERROR_OK;
+}
+
+mal_error_e mal_timer_set_frequency(mal_timer_s *handle, mal_hertz_t frequency, mal_hertz_t delta) {
+    MAL_DEFS_UNUSED(delta);
+    gnu_timers[handle->timer].frequency = frequency;
     return MAL_ERROR_OK;
 }
 
